029ceda9f1cd6e2f2e7def008ddf5e6097b4e9ba
soumyadipghosh23 <gsoumyadip2307@gmail.com>
2022-03-14 14:25:21 +0530

    adding more features

--- static/index.js
+++ static/index.js
@@ -1,5 +1,12 @@
 function change()
 {
+document.getElementById("d").style.display="grid";
+document.getElementById("j").style.display="none";
+
+}
+
+function change1()
+{
 document.getElementById("d").style.display="none";
 document.getElementById("j").style.display="grid";
 
